754484dd15931e9e63b3679225e2f3a64ce3dd33
rakerosion <61892635+rakerosion@users.noreply.github.com>
2020-03-09 19:15:38 -0400

    the new thingy

---
+++ movmatch/form.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<style>
+h1 {
+	font-family: arial;
+}
+
+#header{
+	margin: 0;
+	padding: 0;
+	background-color: yellow;
+}
+#form {
+	width: 60%;
+	height: 2000px;
+	border: 2px solid;
+	border-color: black;
+	margin:auto;
+	text-align: center;
+}
+
+
+
+
+</style>
+</head>
+<body>
+<div id="header">
+<center>
+<h1>MovieMatcher+<h1>
+</div>
+<div id="form">
+<select id="
+</div>
+</center>
+
